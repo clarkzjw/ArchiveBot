746917054567e786c662e2d395438cdf870b8bb7
Jinwei Zhao <clarkzjw@gmail.com>
2016-06-29 23:59:05 +0800

    Update archive.jinja

--- README.md
+++ README.md
@@ -1,25 +1,43 @@
-# ArchiveBot
+ArchiveBot
+==========
 
 Send web links via Telegram bot and archive to webpage.
 
 See https://jinwei.me/archive.html for an example.
 
-## Requirements
+Requirements
+------------
 
-+ [Python3](https://www.python.org/downloads/)
-+ Jinja2
+-	[Python3](https://www.python.org/downloads/)
+-	Jinja2
+
 ```
 pip3 install https://github.com/pallets/jinja/zipball/master
 ```
-+ BeautifulSoup
-```
-pip3 install BeautifulSoup4
-```
-+ [MongoDB](https://www.mongodb.com/download-center?jmp=nav#community)
 
+-	BeautifulSoup`
+	pip3 install BeautifulSoup4
+	`
+-	[MongoDB](https://www.mongodb.com/download-center?jmp=nav#community)
 
-## TODO
+-	PyMongo
 
-+ add archive.is API
-+ Beautify web page UI.
-+ Add delete link function to bot.
+	```
+	pip3 install pymongo
+	```
+
+-	Telepot
+
+	```
+	pip3 install telepot
+	```
+
+	TODO
+	----
+
+-	add archive.is API
+
+-	Beautify web page UI.
+
+-	Add delete link function to bot.
+
--- main.py
+++ main.py
@@ -6,70 +6,73 @@ from bs4 import BeautifulSoup
 from pymongo import MongoClient
 from time import sleep
 
-BOT_TOKEN = '<TOKEN>'
-MONGODB_NAME = "localhost"
-MONGODB_PORT = 27017
+BOT_TOKEN = '***REMOVED***'
+MONGODB_NAME = '***REMOVED***'
 bot = telepot.Bot(BOT_TOKEN)
 
 def UpdateFromTelegram():
-	responses = iter(bot.getUpdates())
+    responses = iter(bot.getUpdates())
 
-	for resp in responses:
-		message_type = resp['message']['entities'][0]['type']
-		message_id = resp['message']['message_id']
-                
-		if message_type == 'url':
-			if FindRecord(message_id) == False:
-				message_url = resp['message']['text']
-				response = urllib.request.urlopen(message_url)
-				data = response.read()
-				soup = BeautifulSoup(data, "lxml")
-				message_title = soup.title.string
+    for resp in responses:
+        # TODO
+        # Check whether every message has 'entities'
 
-				print(message_id, message_url, message_title)
-				InsertDB(message_id, message_url, message_title)
-			else:
-				print("Exist!")
+        message_type = resp['message']['entities'][0]['type']
+        message_id = resp['message']['message_id']
 
-	UpdateHTML()
+        if message_type == 'url':
+            if FindRecord(message_id) == False:
+                message_url = resp['message']['text']
+                response = urllib.request.urlopen(message_url)
+                data = response.read()
+                soup = BeautifulSoup(data, "lxml")
+                message_title = soup.title.string
+
+                print(message_id, message_url, message_title)
+                InsertDB(message_id, message_url, message_title)
+            else:
+                print("Exist!")
+
+    UpdateHTML()
 
 def FindRecord(message_id):
-	Conn = MongoClient(MONGODB_NAME, MONGODB_PORT)
-	database = Conn['archivebot']
-	mycollection = database.entries
-	res = mycollection.find({"id": message_id})
-	if res.count() == 0:
-		return False
-	else:
-		return True
+    Conn = MongoClient(MONGODB_NAME)
+    database = Conn['archivebot']
+    mycollection = database.entries
+    res = mycollection.find({"id": message_id})
+    if res.count() == 0:
+        return False
+    else:
+        return True
 
 def InsertDB(message_id, message_url, message_title):
-	Conn = MongoClient(MONGODB_NAME, MONGODB_PORT)
-	database = Conn['archivebot']
-	mycollection = database.entries
-	post = {"id": message_id, "url": message_url, "title": message_title}
-	mycollection.insert(post)
+    Conn = MongoClient(MONGODB_NAME)
+    database = Conn['archivebot']
+    mycollection = database.entries
+    post = {"id": message_id, "url": message_url, "title": message_title}
+    mycollection.insert(post)
 
 def UpdateHTML():
-	Conn = MongoClient(MONGODB_NAME, MONGODB_PORT)
-	database = Conn['archivebot']
-	mycollection = database.entries
+    Conn = MongoClient(MONGODB_NAME)
+    database = Conn['archivebot']
+    mycollection = database.entries
 
-	templateLoader = jinja2.FileSystemLoader( searchpath="/" )
-	templateEnv = jinja2.Environment( loader=templateLoader )
-	template = templateEnv.get_template("/path/to/ArchiveBot/archive.jinja")
-	
-	templateMessage = []
-	for i in mycollection.find():
-		templateMessage.append({"url": i["url"], "title": i["title"]})
+    templateLoader = jinja2.FileSystemLoader( searchpath="/" )
+    templateEnv = jinja2.Environment( loader=templateLoader )
+    template = templateEnv.get_template("/home/clarkzjw/code/ArchiveBot/archive.jinja")
 
-	templateR = {"messages": templateMessage}
-	output = template.render(templateR)
-	index = open("./archive.html", "w")
-	index.write(output)
-	index.close()
+    templateMessage = []
+    for i in mycollection.find():
+        templateMessage.append({"url": i["url"], "title": i["title"]})
+
+    templateR = {"messages": templateMessage}
+    output = template.render(templateR)
+    index = open("./archive.html", "w")
+    index.write(output)
+    index.close()
 
 if __name__ == "__main__":
-	while(True):
-		UpdateFromTelegram()
-		sleep(10)
+    while(True):
+        print("Update")
+        UpdateFromTelegram()
+        sleep(10)
